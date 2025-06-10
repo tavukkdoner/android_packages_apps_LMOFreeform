242984a3264a26445246dcdcbeb68ded61db3970
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-06-10 08:00:46 +0300

    SidebarService: Hide sidebar when navigationBars is hidden
    In Youtube full screen video, system bar and navigation bar are hidden but sidebar remains active.
    
    Also setOnApplyWindowInsetsListener can not change visibility in real time.

--- sidebar/app/src/main/java/com/libremobileos/sidebar/service/SidebarService.kt
+++ sidebar/app/src/main/java/com/libremobileos/sidebar/service/SidebarService.kt
@@ -16,6 +16,7 @@ import android.os.ServiceManager
 import android.os.UserHandle
 import android.view.View
 import android.view.WindowInsets
+import android.view.WindowInsetsAnimation
 import android.view.WindowManager
 import android.view.WindowManager.LayoutParams
 import android.widget.Toast
@@ -52,14 +53,20 @@ class SidebarService : Service(), SharedPreferences.OnSharedPreferenceChangeList
                 gestureManager.onTouchEvent(event)
                 true
             }
-            setOnApplyWindowInsetsListener { view, insets ->
-                view.visibility = if(!insets.isVisible(WindowInsets.Type.navigationBars())){
-                    View.INVISIBLE
-                } else {
-                    View.VISIBLE
+            val sideView = this
+            setWindowInsetsAnimationCallback(object : WindowInsetsAnimation.Callback(DISPATCH_MODE_CONTINUE_ON_SUBTREE){
+                override fun onProgress(
+                    insets: WindowInsets,
+                    runningAnimations: MutableList<WindowInsetsAnimation>
+                ): WindowInsets {
+                    sideView.visibility = if(!insets.isVisible(WindowInsets.Type.navigationBars())){
+                        View.INVISIBLE
+                    } else {
+                        View.VISIBLE
+                    }
+                    return insets
                 }
-                return@setOnApplyWindowInsetsListener insets
-            }
+            })
         }
     }
 
